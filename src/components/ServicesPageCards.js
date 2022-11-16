86e9ae8ee1e31a1d09edac91fbcb5e1df80dd59a
saifalaasabelaish <saifalaa.099@gmail.com>
2022-11-16 17:12:19 +0200

    New Services Cards Design

---
+++ src/components/ServicesPageCards.js
@@ -0,0 +1,26 @@
+import "./ServicesPageCards.css"
+import img1 from '../images/img1.jpg';
+const Card = (props) => {
+
+    const classes = "card"
+
+    return (
+
+        <div className="col-3">
+            <div className={classes}>
+                <img src={img1} alt ='image 1'/>
+                <div className="card-body">
+                    <h5 className="card-title">{props.title}</h5>
+                    <p className="card-text">
+                        {props.desc}
+                    </p>
+                </div>
+                {props.children}
+            </div>
+        </div>
+
+
+    );
+}
+
+export default Card;
