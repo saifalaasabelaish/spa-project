4086f841d59de233558c24a0c10e0c562babadf1
nourkalbouneh <116949296+nourkalbouneh@users.noreply.github.com>
2022-11-18 23:47:31 +0200

    Wellpage java scripte code

--- src/components/Wellpage.js
+++ src/components/Wellpage.js
@@ -1,60 +1,54 @@
 //import
-import'./components/Wellness.css'
+import'./Wellpage.css'
 import background from './img/image.png';
 
-const Welln =() =>{
+const Wellpage  = () => {
     const classes = "Wellpage"
     return(
-        <>
-        <div className = {classes}>
+        
+        <div className ={classes}>
         <div className= "back">
-    <img src ={background} alt = "back"></img> </div>
-       <div ><p><h2>CON<u>TAC</u>T US</h2></p>
-       <p><h5>ADRESS OF THE SPA</h5></p>
-         </div> 
-       <div className="">
+    <img src ={background} alt = "back"></img> 
+</div>
+    <h1> CONTACT US</h1>
+      <h2>ADDRESS OF THE SPA</h2>
+         
+         
+     <h3> <div class="form-floating mb-3">
+  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"></input>
+  <label for="floatingInput">Email address</label>
+</div>
+</h3>
+<h4>
+<div class="form-floating">
+  <input type="password" class="form-control" id="floatingPassword" placeholder="Password"></input>
+  <label for="floatingPassword">Password</label>
+</div>
+</h4>
 
-        <div className="row">
-<div class="col">
-  <input type="text" className="form-control" placeholder="Your Name" aria-label="Your Name"></input>
+<h5>
+<div class="form-floating">
+  <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" height=" 300px "
+  ></textarea>
+  <label for="floatingTextarea2">Comments</label>
 </div>
-<div className="col">
-  <input type="text" className="form-control" placeholder="Your Email" aria-label="Your Email"></input>
-</div>
-</div>   
+</h5>
+
+<h6 type = "Button">Send Message</h6>
+<div className="footer"><a href="#" class="link-light">FACEBOOK</a>
+<a href="#" class="link-light">TWITTER</a>
+<a href="#" class="link-light"> INSTAGRAME</a>
+
+
 </div>
 
-<div className="">
-<div className="form-floating">
-  <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
-  <label for="floatingTextarea2">Your Message</label>
-</div>
-</div>
-
-<div className="">
-<div className="d-grid gap-2 col-6 mx-auto">
-  <button className="btn btn-primary" type="button">SEND MASSAGE</button>
-</div>
-</div>
-
-<div classNmam="">
-<div className="container">
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <div classNmam="container-fluid">
-      <a classNmam="navbar-brand" href="#">Facebook</a>
-    </div>
-  </nav>
-</div>
-</div>
 
 
 
         </div>
         
 
-</>
-
     )
 }
 
-export default Welln;
+export default Wellpage;
