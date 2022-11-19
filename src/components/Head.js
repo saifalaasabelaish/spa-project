51ffdf222de8197230e29944602baf0653fd62ff
Areen <109797692+areenyaran@users.noreply.github.com>
2022-11-19 05:55:21 +0300

    Update Head.js

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,79 +1,56 @@
 import './Headd.css';
 import headimg from './img/head.png';
 const Head = () => {
-    const classes = "head" 
+    const classes = "headd" 
     return(
-        <div class="d-flex justify-content-center">
-        <div class={classes}>
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <div className={classes}>
+        <div className='headd'>
+        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
+          {/* when i change the id i have problems */}
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
   </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src={headimg} class="d-block w-100" alt="aboutusslide">
-      </img>
-      <div class="carousel-caption d-none d-md-block">
-      <div class='headd'>
-       <h1>if you are looking about beuty you are in the right way!</h1>
-           <h2 type = "Button">Read More About SPA</h2>
-      </div>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src={headimg} class="d-block w-100" alt="bookingslide"></img>
-      <div class="carousel-caption d-none d-md-block">
-      <div class='headd'>
-       <h1>havent booked yet?</h1>
-           <h2 type = "Button">Book Now</h2>
-      </div>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src={headimg} class="d-block w-100" alt="servicesslide"></img>
-      <div class="carousel-caption d-none d-md-block">
-      <div class='headd'>
-       <h1>Enjoy a Facial Treatment at Our Day Spa</h1>
-           <h2 type = "Button">See Oue Services</h2>
-      </div>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src={headimg} class="d-block w-100" alt="productsslide"></img>
-      <div class="carousel-caption d-none d-md-block">
-      <div class='headd'>
-       <h1>We have Many special products for you!</h1>
-           <h2 type = "Button">See Our Products</h2>
-      </div>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src={headimg} class="d-block w-100" alt="offersslide"></img>
-      <div class="carousel-caption d-none d-md-block">
-      <div class='headd'>
-       <h1>What about a gift for yourself?</h1>
-           <h2 type = "Button">Mane offers waits you!</h2>
-      </div>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-        
-        </div>
-        </div>
+  <div className="carousel-inner">
 
+    <div className="carousel-item active">
+      <img src={headimg} className="d-block w-100" alt="aboutusslide"></img>
+      <div class="position-absolute top-50 start-50 translate-middle">
+      <h1>Hi there! If you are looking for beauty , you are in the right place! Welcome to SPA center</h1>
+      <h2 type ="button" class="shadow p-3 mb-5 ">Read More About SPA</h2>
+      </div></div>
+
+    <div className="carousel-item">
+      <img src={headimg} className="d-block w-100" alt="bookingslide"></img>
+      <div class="position-absolute top-50 start-50 translate-middle">
+           <h1>You can book at any time! And you only need <ul> small deposit of the amount!</ul></h1>
+           <h2 type ="button" class="shadow p-3 mb-5 ">Book Now!</h2>
+    </div></div>
+    <div className="carousel-item">
+      <img src={headimg} className="d-block w-100" alt="servicesslide"></img>
+      <div class="position-absolute top-50 start-50 translate-middle">
+       <h1>Enjoy a Facial Treatment at Our Day Spa and <ul>Enhance Your Look with Our Salon Services</ul></h1>
+       <h2 type ="button" class="shadow p-3 mb-5 ">See Oue Services</h2>
+    </div></div>
+    <div className="carousel-item">
+      <img src={headimg} className="d-block w-100" alt="productsslide"></img>
+      <div class="position-absolute top-50 start-50 translate-middle">
+       <h1>We have a lot of reliable global products <ul>that will suit everyone!</ul></h1>
+       <h2 type ="button" class="shadow p-3 mb-5 ">See Our Products</h2>
+    </div></div>
+    <div className="carousel-item">
+      <img src={headimg} className="d-block w-100" alt="offersslide"></img>
+      <div class="position-absolute top-50 start-50 translate-middle">
+       <h1>What if you took a gift for yourself that <ul>includes a variety of everything we own?</ul></h1>
+       <h2 type ="button" class="shadow p-3 mb-5 ">See our offers</h2>
+     </div></div>
+    </div>
+   </div>
+  </div>
+  </div>
     )
 }
 
