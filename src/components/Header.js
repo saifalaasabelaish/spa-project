e69d2d37a38803f197933e5e6020d5828f24b67c
Areen <109797692+areenyaran@users.noreply.github.com>
2022-11-20 21:53:00 +0300

    the final code, home header js code

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,10 @@ import headimg from './img/head.png';
 const Header= (props) => {
     return(
     <>
+     <div className='head'>
     <img src={headimg} className="d-block w-100" alt="homeslider"></img>
     <h1 className="position-absolute top-50 start-50 translate-middle d-inline-flex justify-content-center fs-2 fw-semibold">{props.sentence}</h1>
-    <h2 type ="button" className="position-fixed top-50 start-50 translate-middle-x shadow p-3 col-3 d-inline-flex justify-content-center fs-4">{props.btntitle}</h2>
-    </>
+    <h2 type ="button" className="position-absolute start-50 translate-middle-x shadow p-3 col-3 d-inline-flex justify-content-center fs-4">{props.btntitle}</h2>
+    </div></> 
     )}
     export default Header;
