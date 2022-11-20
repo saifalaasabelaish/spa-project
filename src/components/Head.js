853dd5f04310148e8f192ea5c92d9f31a7f905f1
Areen <109797692+areenyaran@users.noreply.github.com>
2022-11-20 20:35:23 +0300

    Update Head.js

--- src/components/Head.js
+++ src/components/Head.js
@@ -6,15 +6,14 @@ const Head = (props) => {
       <div className={classes}>
       <div className="position-relative d-flex ">
         <div className='head'>
-        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
-          {/* when i change the id i have problems */}
+        <div id="spaproject" className="carousel slide" data-bs-ride="true">
           
   <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+    <button type="button" data-bs-target="#spaproject" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#spaproject" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#spaproject" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#spaproject" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#spaproject" data-bs-slide-to="4" aria-label="Slide 5"></button>
   </div>
   <div className="carousel-inner d-inline-flex">
  
