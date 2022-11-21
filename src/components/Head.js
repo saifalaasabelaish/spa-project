c7c7aa1a7dfb7da7fd8b5ed3774c2b5639833338
Areen <109797692+areenyaran@users.noreply.github.com>
2022-11-21 09:50:36 +0300

    Update Head.js

--- src/components/Head.js
+++ src/components/Head.js
@@ -8,12 +8,12 @@ const Head = (props) => {
         <div className='head'>
         <div id="spaproject" className="carousel slide" data-bs-ride="true">
           
-  <div className="carousel-indicators">
-    <button type="button" data-bs-target="#spaproject" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#spaproject" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#spaproject" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    <button type="button" data-bs-target="#spaproject" data-bs-slide-to="3" aria-label="Slide 4"></button>
-    <button type="button" data-bs-target="#spaproject" data-bs-slide-to="4" aria-label="Slide 5"></button>
+ <div className="carousel-indicators">
+    <button data-bs-target="#spaproject" data-bs-slide-to="0" className="btn active" aria-current="true" aria-label="Slide 1"></button>
+    <button className="btn" data-bs-target="#spaproject" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button className="btn" data-bs-target="#spaproject" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button className="btn" data-bs-target="#spaproject" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button className="btn" data-bs-target="#spaproject" data-bs-slide-to="4" aria-label="Slide 5"></button>
   </div>
   <div className="carousel-inner d-inline-flex">
  
