f684f5c531b00087c68e60d23a5589c614e3e11e
Areen <109797692+areenyaran@users.noreply.github.com>
2022-11-21 19:50:39 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,9 @@ const Header= (props) => {
     <>
      <div className='head'>
     <img src={headimg} className="d-block w-100" alt="homeslider"></img>
-    <div className='container position-absolute top-50 start-50 translate-middle justify-content-center col-6'>
-    <h1 className=" d-inline-flex fs-2 fw-semibold">{props.sentence}</h1>
-    <h2 className=" btn shadow p-3 d-inline-flex justify-content-center fs-4 border ">{props.btntitle}</h2>
+    <div className='container position-absolute top-50 start-50 translate-middle justify-content-center w-50'>
+    <h1 className="d-flex fs-2 fw-semibold justify-content-center mb-5">{props.sentence}</h1>
+    <h2 className="btn rounded-0 shadow p-3 d-flex justify-content-center fs-4 border">{props.btntitle}</h2>
    </div></div></> 
     )}
     export default Header;
