3eebcb7526d7428f978dc46650ace601dc9cb441
wafaahmad <s12144189@stu.najah.edu>
2022-11-23 15:33:31 +0200

    changing the path of the pictures and creating a folder for my work

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
-import './App.css';
-import Staf from './components/Staf';
-function App()
- {
-  return (
- <div> 
-  <Staf/>
- </div> 
-=======
 function App() {
   return (
 <div></div>   
->>>>>>> d63fd1178fbe806db572b281559beb0416ea1396
   );
 }
 export default App
