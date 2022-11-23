a9ee05c10dc2704832972944f3442484081761d8
bayan <bayan7abdalhq@gmail.com>
2022-11-23 16:21:38 +0200

    test

---
+++ src/components/ServApp.js
@@ -0,0 +1,33 @@
+import './components/Serv-Home.css';
+import ServicesHome from './components/ServicesHome.js';
+
+
+
+function App() {
+  return(
+    <>
+    <div className="servises-home">
+        <div className="row-services">
+            <div className="services-title">
+                <h1>SERVICES WE PROVIDE</h1>
+                <p>The spa is first of its kind in palestine in services we provide and profisseinal services we have</p>
+            </div>
+        </div>
+      </div>
+      <div className="container">
+           <div className="row my-3">
+      
+   <ServicesHome ser_card_title="SKIN CARE" ser_card_text="Have you thought before buying cosmetics and skin care products, whether they are suitable for your skin or not? Here are tips for choosing cosmetics for your skin"/>
+   <ServicesHome ser_card_title="BODY CARE" ser_card_text="Whether you're looking for the perfect gift or to treat yourself, shop our Body Care Sale to find all the products you're looking for!" />
+   <ServicesHome ser_card_title="FACE CARE" ser_card_text="A facial is a family of skin care treatments for the face, including steam,  creams, lotions, facial masks, peels, and massage"/>
+   <ServicesHome ser_card_title="HAIR CARE" ser_card_text="Proper and adequate hair care ultimately leads to positive and guaranteed results for your hair and scalp share"/>
+   </div>
+    </div>
+
+
+   </>
+  );
+  
+}
+
+export default App;
---
+++ src/components/ServicesHome.js
@@ -0,0 +1,31 @@
+//import
+import imag1 from './img/care.jpg'
+
+
+//function
+const ServicesHome= (props) =>{
+
+    return(
+        
+      
+            <div className=" col-sm-6 col-md-4 col-lg-3">
+                <div className="services-card" >
+                    <img src={imag1}  alt="skin care"></img>
+                    <div className="card-body">
+                      <h5 className="ser-card-title">{props.ser_card_title}</h5>
+                      <p className="ser-card-text">{props.ser_card_text}</p>
+                      
+                    </div>
+                  </div>
+            </div>
+
+     
+       
+    
+            
+        
+    )
+}
+
+//export
+export default ServicesHome
