25554c1d59a678d5097d2977bbb9a52418cb8d8d
wafaahmad <s12144189@stu.najah.edu>
2022-11-23 19:43:55 +0200

    AddStaffHome java script code

---
+++ src/components/AddStaffHome.js
@@ -0,0 +1,9 @@
+import AddStaf from "./StaffHome/AddStaf"
+import AdStaf from "./StaffHome/AdStaf"
+import Staf from "./StaffHome/Staf"
+const AddStaffHome= () =>{
+    return(<>
+    <Staf/>
+    </>)
+}
+export default AddStaffHome
