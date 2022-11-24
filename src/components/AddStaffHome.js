19229f47c0119313399030fb3585108b0db1f760
saifalaasabelaish <saifalaa.099@gmail.com>
2022-11-24 16:11:08 +0200

    removing the old work

--- src/components/AddStaffHome.js
+++
@@ -1,9 +0,0 @@
-import AddStaf from "./StaffHome/AddStaf"
-import AdStaf from "./StaffHome/AdStaf"
-import Staf from "./StaffHome/Staf"
-const AddStaffHome= () =>{
-    return(<>
-    <Staf/>
-    </>)
-}
-export default AddStaffHome
