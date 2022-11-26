3a29f7aefe53f81829bc931f943dd14ea208bf41
wafaahmad <s12144189@stu.najah.edu>
2022-11-26 16:23:01 +0200

    wellness

---
+++ src/components/ServicesPageTemplete4.js
@@ -0,0 +1,47 @@
+import './ServicesPageTemplete.css'
+const ServicesPageTemplete4= () =>
+{ 
+    return(
+    <><div  className='ServicesPage' >
+    <div className="justify-conten d-white  tbg-body  bg-white rounded callout shadow h-100 w-100 p-4 mb-200"> 
+       <div className=' container start-10 col-19 '>
+     <div className="ServicesPageTemplete "> 
+<div className="titil-services">
+        <img src='./images/wellness1.jpg' className="img" alt="wellness "></img>
+        </div>  </div>  
+          <div className="item-wrapper"><h4>Wellness</h4>
+            <div className="title"><h5>About Us</h5></div>
+              <p>
+                Wellness involves the awareness of our current state of health in multiple
+                 dimensions with the initiative, 
+                tools and support to make lasting changes towards a more optimal life.
+                </p></div>  
+                 <div className="item-wrapper">
+                <div className="title">
+                    <h5>Wellness Consultation</h5></div>
+                <div className="text-area">
+                                 <p>It fortified with keratin which can lengthen and protect your lashes from day to day.
+                                 It not only stimulates growth for healthy and strong eyelashes; but also add strength and support to those weak ones.
+                                 Plumps up thin, sparse lashes to full, lushest look in just 21 days! Seals and protects weak lashes against splitting and 
+                                 breaking. The high performance brush is made of many rigid bristles to lengthen every little hard-to-reach lashes, 
+                                 so all lashes look their longest!</p>
+                                 </div></div>  <div className="item-wrapper">
+                <div className="title"><h5>WELLNESS FOLLOW-UP</h5></div>
+                <div className="text-area">
+                                 <p>
+                                  A 30-minute fine tuning session. A time to ask questions, note progress and adjust goals.
+                                   Only available after initial consultation.</p>
+                                 </div></div><div className="item-wrapper">
+                <div className="title"><h5>WELLNESS PACKAGE</h5></div>
+                <div className="text-area">
+                                 <p>
+                                 Includes a 60-minute consultation on topics of your choice and four follow-up sessions that can be scheduled
+                                  at your convenience (weekly, bi-monthly, monthly)</p>
+                                 </div></div></div></div>
+                     </div>
+
+    </>)
+}
+
+export default ServicesPageTemplete4 
+
