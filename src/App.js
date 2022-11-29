adfd315bb3c63da488d262fdbcc1a8dd7119b868
saifalaasabelaish <saifalaa.099@gmail.com>
2022-11-29 21:24:42 +0200

    testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
+import HomePage from "./components/HomePage"
 function App() {
   return (
 <>
-<Head/>
+<HomePage></HomePage>
 </> 
   );
 }
 
 export default App;
-=======
-
-   
-    
-    function App() {
-      return(
-       <div></div>
-          
-       
-      );
-      
-    }
-    
-    export default App;  
->>>>>>> 27dc8a5d6fbe1bd17a88795a3fb99c3d60639c22
