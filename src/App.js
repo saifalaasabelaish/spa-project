59c84adc46e00270c62f6ef1011692912891375a
wafaahmad <s12144189@stu.najah.edu>
2022-12-01 20:25:07 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,38 +1,5 @@
-<<<<<<< HEAD
-import ServicesPageTemplete1 from "./components/ServicesPageTemplete1"
-import ServicesPageTemplete2 from "./components/ServicesPageTemplete2"
-import ServicesPageTemplete3 from "./components/ServicesPageTemplete3"
-import ServicesPageTemplete4 from "./components/ServicesPageTemplete4" 
- function App()
- {
-  return (
-<div>
- <ServicesPageTemplete3/>
-  </div>
- 
-  );
-}
-export default App
- 
-=======
-<<<<<<< HEAD
+
 import './App.css';
-import Navbar from './components/Navbar';
-
-function App() {
-  return (
-<div>
-  <Navbar>
-    
-  </Navbar>
-</div>   
-  );
-}
-
-export default App;
-=======
-
-   
     
     function App() {
       return(
@@ -43,6 +10,4 @@ export default App;
       
     }
     
-    export default App;  
->>>>>>> 27dc8a5d6fbe1bd17a88795a3fb99c3d60639c22
->>>>>>> d0b3ce240baecbfaea6fd68f328125be42a9d745
+    export default App; 
