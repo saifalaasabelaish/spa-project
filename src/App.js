74e84f83907b18aa25e858a52d29d91023a5d118
lial-adam <s11926390@stu.najah.eduu>
2022-12-01 19:48:02 +0200

    App

--- src/App.js
+++ src/App.js
@@ -28,3 +28,16 @@ export default App;
     
     export default App;  
 >>>>>>> 27dc8a5d6fbe1bd17a88795a3fb99c3d60639c22
+import Navbar from "./components/Navbar.js";
+
+
+function App() {
+  return (
+    <Navbar>
+      
+<h1>ddd</h1>
+</Navbar>
+  );
+}
+
+export default App;
