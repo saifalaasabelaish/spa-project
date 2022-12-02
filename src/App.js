2955b64a5166c9573a712ec08d71e12c06b70d67
Roaajadaa <116982318+Roaajadaa@users.noreply.github.com>
2022-12-02 15:45:27 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-<<<<<<< HEAD
+
 import './App.css';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
 <div>
-  <Navbar>
-    
-  </Navbar>
+ 
 </div>   
   );
 }
-
 export default App;
-=======
-
-   
-    
-    function App() {
-      return(
-       <div></div>
-          
-       
-      );
-      
-    }
-    
-    export default App;  
->>>>>>> 27dc8a5d6fbe1bd17a88795a3fb99c3d60639c22
--- src/components/testRoaa/Shoppinglabel.js
+++ src/components/testRoaa/Shoppinglabel.js
@@ -9,25 +9,37 @@ const Shoppinglabel = () =>
 <Shopping/>
 
 <h3 className = " header2 d-flex justify-content-center" > S H O P P I N G &nbsp; P A G E </h3>
-<div className ="countiner">
- <div className = "row" > 
-    <div className = "col-2 border shadow p-3 mb-5 bg-body rounded group"> 
-<div class=" btn-group-vertical " role="group" aria-label="Vertical button group" >
+
+
+<div className= "countiner">
+  <div className = "row">
+    <div className = "col-3">
+<div className="card2  border shadow p-3 group d-flex ">
+  <div className="card-body ">
+<div className=" btn-group-vertical " role="group" aria-label="Vertical button group" >
 <h6> PRODUCT <br/> CATEGORIES</h6>
-  <button type="button" class="btn p-3 ">Face masks</button>
-  <button type="button" class="btn p-3 "> Foundation </button>
-  <button type="button" class="btn p-3"> Make up</button>
-  <button type="button" class="btn p-3">Body products </button>
-  <button type="button" class="btn p-3"> Perfumes </button>
-  <button type="button" class="btn p-3"> Hair products </button>
+  <button type="button" className="btn p-3 ">Skin Care </button>
+  <button type="button" className="btn p-3 "> Foundation </button>
+  <button type="button" className="btn p-3"> Make up</button>
+  <button type="button" className="btn p-3">Body products </button>
+  <button type="button" className="btn p-3"> Perfumes </button>
+  <button type="button" className="btn p-3"> Hair products </button>
+  </div>
   </div>
   </div>
 </div>
+<div className ="col-3">
+<div className="card3 border ">
+  <img src=" ./mask3.jpg" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <p className="card-text ">  Face Wash  </p>
+    <p > 48.00 $</p>
   </div>
- 
+</div>
 
-
-
+</div>
+</div>
+</div>
 </>
     )
 };
