eea926b913b52169613c298d609e6f43973b5465
saifalaasabelaish <saifalaa.099@gmail.com>
2022-12-04 19:41:42 +0200

    Daily Push

--- src/components/BookingPage/Booking.js
+++ src/components/BookingPage/Booking.js
@@ -8,10 +8,15 @@ const BookingPage = () => {
     return (
         <>
             <BookingTexts></BookingTexts>
-            <BookingForm></BookingForm>
-            <BookNowButton></BookNowButton>
-
-
+            <div className="container ">
+                <div className="row vh-100 align-items-center justify-content-center">
+                    <div className="col-12 col-md-8 col-sm-8 col-lg-5 bg-white rounded p-4 shadow">
+                        <BookingForm>
+                            <BookNowButton></BookNowButton>
+                        </BookingForm>
+                    </div>
+                </div>
+            </div>
         </>
 
 
--- src/components/BookingPage/BookingForm.js
+++ src/components/BookingPage/BookingForm.js
@@ -1,38 +1,30 @@
 const BookingForm = (props) => {
-
-
     return (
 
         <>
-            <div className="container ">
-                <div className="row vh-100 align-items-center justify-content-center">
-                    <div className="col-12 col-md-8 col-sm-8 col-lg-5 bg-white rounded p-4 shadow">
-                        <form>
-                            <div className="row">
-                                <h1> Customer Information</h1>
-                                <div className="col-md-6 mb-3">
-                                    <label for="exampleInputName" className="form-label">Full Name</label>
-                                    <input type="name" className="form-control" aria-describedby="nameHelp" />
-                                </div>
-                                <div className="col-md-6 mb-3">
-                                    <label for="exampleInputNumber" className="form-label">Phone Number</label>
-                                    <input type="name" className="form-control" aria-describedby="numberHelp" />
-                                </div>
-                            </div>
-                            <div className="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                            </div>
-                            <div className="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                            </div>
-                            {props.children}
-
-                        </form>
+            <form>
+                <div className="row">
+                    <h1> Customer Information</h1>
+                    <div className="col-md-6 mb-3">
+                        <label for="exampleInputName" className="form-label">Full Name</label>
+                        <input type="name" className="form-control" aria-describedby="nameHelp" />
+                    </div>
+                    <div className="col-md-6 mb-3">
+                        <label for="exampleInputNumber" className="form-label">Phone Number</label>
+                        <input type="name" className="form-control" aria-describedby="numberHelp" />
                     </div>
                 </div>
-            </div>
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                </div>
+                {props.children}
+            </form>
+
         </>
     )
 }
