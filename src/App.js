4db311c75d8259acb7318547e2c0067a516e8224
lial-adam <s11926390@stu.najah.eduu>
2022-12-05 23:17:36 +0200

    app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-function App() {
+import React from 'react';
+import Navbar from './components/common/Navbar.js';
+import HomePage from "./components/Pages/HomePage"
+import Contact from "./components/Pages/ContactUsPage";
+import ServicesPage from "./components/Pages/ServicesPage";
+import About from './components/Pages/About.js';
+
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+
+
+    
+export default function App() {
   return (
-    <>
-    </>
+    <Router>
+      <Navbar/>
+      <ServicesPage/>
+      <Contact/>
+      <HomePage/>
+      <About/>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/About">About Us</Link>
+            </li>
+            <li>
+              <Link to="/Contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/ServicesPage">Services</Link>
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
 
+        <Routes>
+          <Route path="/About" element={<About />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/ServicesPage" element={<ServicesPage />} />
+
+        </Routes>
+      </div>
+    </Router>
   );
-
 }
 
-export default App; 
