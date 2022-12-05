3ce70f59401b3654492322d6c713278e78be4119
saifalaasabelaish <saifalaa.099@gmail.com>
2022-12-05 16:54:25 +0200

    weekly merge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-<<<<<<< HEAD
 function App() {
   return (
-    <div></div>
+    <>
 
+    </>
 
   );
 
 }
 
-export default App;  
-=======
-
-    function App()
-     {
-      return(
-<>
- 
-</>
-             
-      );
-      
-    }
-    
-    export default App; 
->>>>>>> wafaa
+export default App; 
