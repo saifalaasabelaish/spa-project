8fe867398276d173fda8aec52fc3e37cd152c97c
Roaajadaa <116982318+Roaajadaa@users.noreply.github.com>
2022-12-07 17:57:25 +0200

    for all

---
+++ src/components/common/HeaderPage.js
@@ -0,0 +1,10 @@
+
+
+import './HeaderPage.css'
+const HeaderPage = () => {
+return(
+<h3 className = " header2 d-flex justify-content-center" >{/*WRITE YOUR NAME PAGE WITH UPPER CASE FOR EACH LETTER*/}</h3>
+)
+}
+
+export default HeaderPage;
