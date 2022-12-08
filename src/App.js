f3c78c8e0fd21a780a667224cbfd00216eaeed25
wafaahmad <s12144189@stu.najah.edu>
2022-12-08 19:10:34 +0200

    pull

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+
+
+import  ServicesPageTemplete1 from "./components/ServicesPageTemplete/ServicesPageTemplete1";
+import  ServicesPageTemplete3 from "./components/ServicesPageTemplete/ServicesPageTemplete3";
+import  ServicesPageTemplete2 from "./components/ServicesPageTemplete/ServicesPageTemplete2";
+import  ServicesPageTemplete4 from "./components/ServicesPageTemplete/ServicesPageTemplete4";
+import  Staff  from "./components/StaffHome/Staff"
+
 function App() {
   return (
-    <>    
-    </>
-
+    <> 
+    <ServicesPageTemplete1/>
+ 
+</>
 
   );
 
