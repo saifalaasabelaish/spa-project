6a6e5da21fa99dd04e75068e97d58fa26857b891
nourkalbouneh <116949296+nourkalbouneh@users.noreply.github.com>
2022-12-09 12:22:22 +0200

    java script and css code

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,21 +1,24 @@
 import './AboutUs.css'
 import Footer from '../common/Footer'
+import Navbar from '../common/Navbar'
 const AboutUs = () =>
 {
  return(
  <> 
- <center>
- <h1>History</h1>
- <div className='h-20 w-50'>
- <p>Whilst many people associate traditional spas with Roman baths,
+ <Navbar/>
+ <div className='container'>
+ <div className='center'>
+ <h1 className='text-center'>History</h1>
+ <div className='text-center fs-5 justify-content-center w-150 mx-4'>
+ <p >Whilst many people associate traditional spas with Roman baths,
   
    there is evidence of spa-type therapies dating back thousands of years when there was a
     belief in the curative powers of mineral waters. Paul Joseph, co-founder of Health and
      Fitness Travel explains: “Spas, healing waters, thalassotherapy, hydrotherapy and hot 
      springs date back thousands of years - an ancient practice conducted long before the Greeks and Romans!”</p>
-</div></center>
+</div>
 <div className="about">
-<center> <h1>Team of spa</h1></center>
+ <h1 className='text-center'>Team of spa</h1>
         <div className="card-group w-50">
   <div className="card">
     <img src='./images/nn.webp' className="card-img-top w-60 " alt="..."></img>
@@ -42,7 +45,7 @@ const AboutUs = () =>
     </div>
   </div>
 </div>
-<center><h1>Founders of spa</h1></center>
+<h1 className='text-center'>Founders of spa</h1>
         <div className="card-group w-50 ">
   <div className="card">
     <img src='./images/nj.jpg ' className="card-img-top w-60 " alt="..."></img>
@@ -53,7 +56,7 @@ const AboutUs = () =>
     </div>
   </div>
   <div className="card">
-    <img src='./images/ni.jpg' className="card-img-top w-60" alt="..."></img>
+    <img src='./images/ni.jpg' className="img " alt="..."></img>
     <div className="card-body">
       <h5 className="card-title">Jo kateb</h5>
       <p className="card-text">investor</p>
@@ -62,11 +65,15 @@ const AboutUs = () =>
   </div>
       
 </div> 
-<center><h2>Location</h2></center>
 
-<Footer/>
+<h2 className='text-center'>Location</h2>
+<iframe className="gmap_iframe"  frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=najah&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
 
 </div>
+</div>
+</div>
+<Footer/>
+
  </>
  )
 }
@@ -76,8 +83,4 @@ export default AboutUs;
 
 
 
-            
-            {/* 
-            
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3375.257108141327!2d35.246620784876505!3d32.224242981142446!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151ce19096423d5f%3A0x34306c6102b97f21!2sNagham.Pro!5e0!3m2!1sar!2s!4v1670268167677!5m2!1sar!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-*/}
+          
