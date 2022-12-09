c94e83151d414f99ca2d5ea190a8295150922eb1
wafaahmad <s12144189@stu.najah.edu>
2022-12-09 21:37:11 +0200

    ServicesPageTemplete

---
+++ src/components/ServicesPageTemplete/SkinCare.js
@@ -0,0 +1,35 @@
+import './ServicesPageTemplete.css'
+import ServicesPageTemplete from './ServicesPageTemplete'
+import Navbar from '../common/Navbar'
+import Footer from '../common/Footer'
+const SkinCare = () =>
+{ 
+    return(
+    <>
+   
+    <div className="ServicesPageTemplete "> <Navbar/>
+    < ServicesPageTemplete imgg= './images/skin care2.png'  alt="skin care"
+        title1="About Us" Paragraph1=" is a range of practices that support skin integrity, enhance its appearance, and relieve skin conditions.
+        They can include nutrition, avoidance of excessive sun exposure, and appropriate use of emollients.
+        Practices that enhance appearance include the use of cosmetics, botulinum, exfoliation, fillers, laser resurfacing,
+        microdermabrasion, peels, retinol therapy, and ultrasonic skin treatment. Skin care is a routine daily procedure in many settings,
+        such as skin that is either too dry or too moist, and prevention of dermatitis
+        and prevention of skin injuries"
+        title2="Background" Paragraph2="Skin care is at the interface of cosmetics and dermatology.
+        The US Federal Food, Drug, and Cosmetic Act defines cosmetics as products intended to cleanse or beautify
+        (for instance, shampoos and lipstick). A separate category exists for medications, which are intended to diagnose, cure, mitigate, treat, or prevent disease, or to affect the structure or function of the body (for instance, sunscreens and acne creams), although some products, such as moisturizing sunscreens and anti-dandruff shampoos, are regulated within both categories.
+        Skin care differs from dermatology by its inclusion of non-physician professionals, such as estheticians and wound care nursing staff.Skin care includes modifications of individual behavior and of environmental and working conditions." 
+        title3="Neonate" Paragraph3=" Guidelines for neonatal skin care have been developed.
+        Nevertheless, the pediatric and dermatological communities have not reached a consensus on best cleansing practices,
+        as good quality scientific evidence is scarce. Immersion in water seems superior to washing alone, and use of synthetic
+        detergents or mild liquid baby cleansers seems comparable or superior to water alone. Add from ."
+        title4="Radiation" Paragraph4=" Radiation induces skin reactions in the treated area, particularly in the axilla, head and neck, perineum and skin fold regions.Formulations with moisturising, anti-inflammatory, anti-microbial and wound healing properties are often used, but no preferred approach or individual product has been identified as best practice. Soft silicone dressings that act as barriers to friction may be helpful.
+        In breast cancer, calendula cream may reduce the severity of radiation effects on the dark spot corrector.Deodorant use after completing radiation treatment has been controversial but is now recommended for practice.Add from"/> 
+    <Footer/> </div>
+     
+               </>)
+            }
+            
+            export default SkinCare
+            
+            
