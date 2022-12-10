92198fac2ba78d979bc7adc197d963771d1674b0
Areen <109797692+areenyaran@users.noreply.github.com>
2022-12-11 01:30:40 +0200

    edit code

---
+++ src/components/AboutUsPage/AboutUsCard.js
@@ -0,0 +1,21 @@
+import './AboutPage.css'
+const AboutUsCard = (props) => {
+  return (
+    <>
+      <div className='container'>
+        <div className='center'>
+
+          <div className="card">
+            <img src={props.img1} className="img card-img-top w-60" alt={props.img1}></img>
+            <div className='write'>
+
+              <h5 className="card-title">{props.title}</h5>
+              <p className="card-text">{props.Paragraph}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+export default AboutUsCard
