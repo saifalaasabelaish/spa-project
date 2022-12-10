47ffbaac64f4bf1fb07b5505ca1f3f807e491af0
Areen <109797692+areenyaran@users.noreply.github.com>
2022-12-11 01:31:14 +0200

    edit footer & nav

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,13 +1,31 @@
 import "./Navbar.css";
-const Navbar=()=>{
-    return(
-       
-        
-      <div className="col-xs-6 col-md-2">
-      <img id="Logo" src="/images/SPA_Logo.png" alt="Logo"/>                  
- </div>
- 
-
-    );
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    // Areen work 
+    <div className="navbarr col-12 ">
+      <nav>
+        <ul>
+          <div className='navbar-brand logo position-absolute start-0 m-3'>
+            <img img src="/images/spalogo.png" alt="Logo"></img>
+          </div>
+          <div className="rightfloat">
+            <li>
+              <Link to="/Contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/ServicesPage">Services</Link>
+            </li>
+            <li>
+              <Link to="/About">About Us</Link>
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </div>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 export default Navbar;
