90d391084bedf8d03bad1f9b6aa0f73e19f8ea44
Areen <109797692+areenyaran@users.noreply.github.com>
2022-12-11 22:16:48 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
 import React from 'react';
-import Navbar from './components/common/Navbar.js';
-import HomePage from "./components/pages/HomePage"
-import Contact from "./components/pages/ContactUsPage";
-import ServicesPage from "./components/pages/ServicesPage";
-import About from './components/pages/About';
-
-import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import HomePage from "./components/Pages/HomePage"
+import ContactPage from './components/Pages/ContactUsPage';
+import ServicesPage from "./components/Pages/ServicesPage";
+import AboutUsPage  from './components/Pages/AboutUsPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
     <Router>
-   <Navbar/>
-      
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/About">About Us</Link>
-            </li>
-            <li>
-              <Link to="/Contact">Contact</Link>
-            </li>
-            <li>
-              <Link to="/ServicesPage">Services</Link>
-            </li>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-          </ul>
-        </nav>
         <Routes>
-          <Route path="/About" element={<About />} />
+          <Route path="/About" element={<AboutUsPage  />} />
           <Route path="/" element={<HomePage />} />
-          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Contact" element={<ContactPage />} />
           <Route path="/ServicesPage" element={<ServicesPage />} />
-
         </Routes>
       </div>
     </Router>
