2f5262bcbaa5bfa0d9e6c258c2df0df23fa18009
wafaahmad <s12144189@stu.najah.edu>
2022-12-11 23:53:11 +0200

    tast

--- src/components/StaffHome/StaffHome.js
+++ src/components/StaffHome/StaffHome.js
@@ -7,7 +7,8 @@ const StaffHome= (props) =>
       <div className="staff col-12 d-flex p-5">
    <div className='staffhome'> <img src={props.img} className="img-top img-fluid six-col rounded-circle  mx-2 " alt={props.alt1}></img>
    <div className="titleStaff text-center mt-3">
-    <h5 >{props.title}</h5><p>{props.paragraph1}</p> {props.children} </div></div></div>
+    <h5 >{props.title}</h5></div><div className="paragraphStaff  mt-3">
+    <p>{props.paragraph1}</p> {props.children} </div></div></div>
       </>  
      
     )}
