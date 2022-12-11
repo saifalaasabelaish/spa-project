217615d6797390a5e61eff28fa21d47d646e0c06
wafaahmad <s12144189@stu.najah.edu>
2022-12-11 21:59:40 +0200

    tast

--- src/components/StaffHome/Staff.js
+++ src/components/StaffHome/Staff.js
@@ -8,18 +8,18 @@ const Staff= () =>
     <section>
     <div className="staff align-items-center col-12 d-flex p-5">
       <h1 className='p-4'>MEET OUR BEAUTIFUL TEAM</h1>
-      <div className=" container justify-content-center" >
-      <div className="row align-items-center">
-      <div className="col-sm-6 col-md-4 col-lg-3 "> 
+      <div className=" container " >
+      <div className="row">
+      <div className=" col-md-4 col-lg-3"> 
         < StaffHome img='./images/shayne.jpeg' alt1="first" title="Shayne Crocker" paragraph1 ="I'm Shane Crocker,48 years old.work in massage ,Skilled in various modalities 
-      of massage including Swedish, Deep Tissue/Neuromuscular</p><div className='text-center" ><SocialMedia1/></StaffHome></div>
-     <div className="col-sm-6 col-md-4 col-lg-3 "> 
+      of massage including Swedish, Deep Tissue/Neuromuscular"><SocialMedia1/></StaffHome></div>
+     <div className=" col-lg-3 "> 
      <StaffHome  img='./images/megan.jpg' alt1="scond" title="Megan Martin" paragraph1 ="I am Megan Martin,27 years old working in makeup ,Skilled in Swedish Massage, Deep Tissue Massage and Sports Massage.
 Practices integrative massage that combines techniques to form." ><SocialMedia2/></StaffHome></div>
- <div className="col-sm-6 col-md-4 col-lg-3 "> 
+ <div className=" col-md-4 col-lg-3 "> 
      <StaffHome  img='./images/amanda.jpg' alt1="three" title="Amanda Mollett" paragraph1 ="I am Amanda Mollett,35 years old working in massage ,Specialize in Craniosacral, Neuromuscular,
    Swedish, Deep Tissue, migraine and TMJD work."><SocialMedia1/></StaffHome></div>
-   <div className="col-sm-6 col-md-4 col-lg-3 "> 
+   <div className=" col-md-4 col-lg-3 "> 
    <StaffHome  img='./images/lydia.jpg' alt1="four" title="Lydia Smothers" paragraph1 ="I am Lydia Smothers ,30 years old I have been working in massage for 10 years,Skilled in Swedish Massage, Deep Tissue Massage and Sports Massage.
 Practices integrative massage that combines techniques to form. "><SocialMedia2/></StaffHome></div>
   </div></div></div>
--- src/components/StaffHome/StaffHome.js
+++ src/components/StaffHome/StaffHome.js
@@ -5,7 +5,7 @@ const StaffHome= (props) =>
 { 
     return(<>
       <div className="staff col-12 d-flex p-5">
-   <div className='card '> <img src={props.img} className=" img-fluid six-col rounded-circle  mx-2" alt={props.alt1}></img>
+   <div className='staffhome'> <img src={props.img} className="img-top img-fluid six-col rounded-circle  mx-2 " alt={props.alt1}></img>
    <div className="titleStaff text-center mt-3">
     <h5 >{props.title}</h5><p>{props.paragraph1}</p> {props.children} </div></div></div>
       </>  
