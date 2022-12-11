2d3a486ebc807aa97a0d87a95c796ab6bd8c7af6
wafaahmad <s12144189@stu.najah.edu>
2022-12-11 16:35:43 +0200

    test

---
+++ src/components/StaffHome/StaffHome.js
@@ -0,0 +1,15 @@
+import './Staff.css'
+import SocialMedia1 from './SocialMedia1'
+import SocialMedia2 from './SocialMedia2'
+const StaffHome= (props) =>
+{ 
+    return(<>
+      <div className="staff col-12 d-flex p-5">
+   <div className='card '> <img src={props.img} className=" img-fluid six-col rounded-circle  mx-2" alt={props.alt1}></img>
+   <div className="titleStaff text-center mt-3">
+    <h5 >{props.title}</h5><p>{props.paragraph1}</p> {props.children} </div></div></div>
+      </>  
+     
+    )}
+
+export default StaffHome
