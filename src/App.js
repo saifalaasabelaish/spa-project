4c70cd5245ca18fb757cf1be092c84edfe1812af
Roaajadaa <116982318+Roaajadaa@users.noreply.github.com>
2022-12-11 23:57:18 +0200

    Pushing the shopping page updates

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
 import React from 'react';
 import Navbar from './components/common/Navbar';
 import HomePage from "./components/Pages/ContactUsPage"
-import Contact from "./components/pages/ContactUsPage";
-import ServicesPage from "./components/pages/ServicesPage";
-import About from './components/pages/About';
+import Contact from "./components/Pages/ServicesPage";
+import ServicesPage from "./components/Pages/About";
+import About from './components/Pages/HomePage';
 
 import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
 
 export default function App() {
   return (
     <Router>
-   <Navbar/>
-      
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/About">About Us</Link>
-            </li>
-            <li>
-              <Link to="/Contact">Contact</Link>
-            </li>
-            <li>
-              <Link to="/ServicesPage">Services</Link>
-            </li>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-          </ul>
-        </nav>
         <Routes>
           <Route path="/About" element={<About />} />
           <Route path="/" element={<HomePage />} />
@@ -36,7 +17,7 @@ export default function App() {
           <Route path="/ServicesPage" element={<ServicesPage />} />
 
         </Routes>
-      </div>
+    
     </Router>
   );
 }
