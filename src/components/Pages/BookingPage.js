a32ae489ae30efe35d8644d94dfb5978de2017b7
saifalaasabelaish <saifalaa.099@gmail.com>
2022-12-15 19:20:11 +0200

    final merge of the week

--- src/components/Pages/BookingPage.js
+++ src/components/Pages/BookingPage.js
@@ -7,16 +7,18 @@ import Navbar from "../common/Navbar"
 const BookingPage = () => {
     return (
         <>
-        <div className="booking col-md-12 col-sm-12 col-xxl-12 col-lg-12 d-flex">
-        <img src="images/booking.png" alt="booking page" className="position-fixed vh-100 w-100 img-fluid d-flex"></img>
-        <div className="position-absolute top-0 col-12 col-md-12 col-sm-12 ">
-        <Navbar/>
-        <HeaderPage title ="BOOKING PAGE"/>
-        <div className="row w-100 align-items-center justify-content-center">
-        <div className="mt-2 col-md-8 col-sm-8 col-lg-7 rounded bg-white p-4 shadow">
-
-        <BookingForm><div className="text-end"><BookNowButton/></div></BookingForm>
-       </div> </div></div>
-       </div>
-       </>);}
- export default BookingPage;
+            <div className="booking col-md-12 col-sm-12 col-xxl-12 col-lg-12 d-flex">
+                <img src="images/booking.png" alt="booking page" className="position-fixed vh-100 w-100 img-fluid d-flex"></img>
+                <div className="position-absolute top-0 col-12 col-md-12 col-sm-12 ">
+                    <Navbar />
+                    <HeaderPage title="BOOKING PAGE" />
+                    <div className="row w-100 align-items-center justify-content-center mb-5">
+                        <div className="mt-2 col-md-8 col-sm-8 col-lg-7 rounded bg-white p-4 shadow">
+                            <BookingForm><div className="text-end"><BookNowButton /></div></BookingForm>
+                        </div>
+                    </div>
+                    <Footer />
+                </div>
+            </div></>);
+}
+export default BookingPage;
