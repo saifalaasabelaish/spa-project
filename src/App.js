c4b4ae93a2d55815e3e570912503f0bd6909be54
saifalaasabelaish <saifalaa.099@gmail.com>
2022-12-15 18:00:14 +0200

    testing bayan's work

--- src/App.js
+++ src/App.js
@@ -1,41 +1,8 @@
-<<<<<<< HEAD
-import React from 'react';
-import HomePage from "./components/Pages/HomePage"
-import ContactPage from './components/Pages/ContactUsPage';
-import ServicesPage from "./components/Pages/ServicesPage";
-import AboutUsPage  from './components/Pages/AboutUsPage';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ShoppingPage from './components/Pages/ShoppingPage';
-import BookingPage from './components/Pages/BookingPage';
-
-export default function App() {
-  return (
-    <Router>
-      
-        <Routes>
-          <Route path="/About" element={<AboutUsPage  />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="/Contact" element={<ContactPage />} />
-          <Route path="/ServicesPage" element={<ServicesPage />} />
-          <Route path="/ShoppingPage" element={<ShoppingPage />} />
-          <Route path="/BookingPage" element={<BookingPage/>} />
-        </Routes>
-    
-    </Router>
-  );
-}
-=======
-import Shoppinglabel from "./components/Pages/ShoppingPage";
-
-
-
-
-
-
+import ServicesPage from "./components/Pages/ServicesPage"
     function App() {
       return(
       <div>
-        <Shoppinglabel/>
+        <ServicesPage></ServicesPage>
       </div>
       );
       
@@ -43,4 +10,3 @@ import Shoppinglabel from "./components/Pages/ShoppingPage";
       
     
     export default App;
->>>>>>> 37ac6e06a0896a87d69da9407fd804c13e0bfa0f
