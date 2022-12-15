6af2e113b36c0a9f85f11d4222c66472052c2833
Areen <109797692+areenyaran@users.noreply.github.com>
2022-12-15 19:22:28 +0200

    pull main

--- src/components/Pages/ServicesPage.js
+++ src/components/Pages/ServicesPage.js
@@ -4,21 +4,8 @@ import React, { useState } from "react";
 import Footer from "../common/Footer";
 import Services from '../ServicesPage/Services';
 import HeaderPage from '../common/HeaderPage';
+import Navbar from '../common/Navbar';
 
-const Cart = (onClick , props) =>
-  {
-  return (
-    <>
-    <div className="cart  justify-content-end d-flex me-4 mt-3">
-      <button  className ="cart-btn " onClick={() => onClick()}>
-      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAYAAADhAJiYAAAAAXNSR0IArs4c6QAAAdJJREFUWEftl70uBFEYhp9N0KJW+GloJOIG0GjFFfgp6GQvgJBwAego/NwBrQY3IBENjZ9Cr0ZCXjmzOTO7M3PmzBm7xU63O+f7vme+95v3nKnRYVetw3joAuUpkuzQDvBjBd0At3lJQt5PAtkwUZ0zYCVk0axcLkCKXwQu/gOqlWRR3SlgwfyQdHPtALJrjgAv1h+jwGvVUHmvveZnyUCcA8vtBpoFrg3EB7BfAZAetNH5vA6pvhYPVwASpVR+jcPf5QIkmU4rBHoDNK/OQAOmS/0VQR0A9SJAWmsPd2iu2NvrIpkAkhYQCuoekN81LlcgBcgcZ0KRmDzaktR9L6AqhnsQkJ14ASkopAW0NNoikglIx5PtQLK13LCLAoUa7pj3lJEslAXEvKcskL2/+aqXenIoKlkEUGa4m7ynbIcUX8YCmrwnBFAE1dgUHbVTZ2NGmIzzlcyxfvFlZYCOzNfIM7ABXKWUnwcOgTFzjFnPwvQF0hycWImfgImUQo/AuHVvNet85Qu0CexaRb6AvhSgT6DXurcF7KV1yRdoErgDekziYyBNCkm7ZtZ9A9PAQ2gg5ROUvtveHY64kngIuMyCUVLfDhV/fRwjukB5jeq4Dv0CIkZFJZM8NK0AAAAASUVORK5CYII=" />
-      <span className="badge text-bg-secondary border-5">  { props.datap } </span>
-      </button>
-    </div>
-    </>
-    
-  )
-}
 const ServicesPage = () => {
   const [category, setCategory] = useState("skin-care");
   const eventHandler = (event) =>
@@ -28,17 +15,17 @@ const ServicesPage = () => {
 
   return (
     <>
-      {/* <Shopping /> */}
+   <Navbar/>
       <HeaderPage title =" Services "/>
-      <Cart/>
+
       <div className="container">
         <div className="row">
-          {/* categories */}
+       
           <div className="col-12 col-md-4 col-lg-3 col-xxl-2">
             <div className="card2 border shadow p-3 group d-flex ">
               <div className="card-body ">
                 <div className=" btn-group-vertical d-flex justify-content-center" role="group" aria-label="Vertical button group" >
-                  < h6 className = "headercategory">PRODUCT <br/> CATEGORIES</h6>
+                  < h6 className = "headercategory">PRODUCT CATEGORIES</h6>
                   <button type="button" onClick={eventHandler} className="btn p-4 " id="skin-care">Skin Care </button>
                   <button type="button" onClick={eventHandler} className="btn p-4" id="salon-services">Salon Services </button>
                   <button type="button" onClick={eventHandler} className="btn p-4" id="massage">Massage </button>
