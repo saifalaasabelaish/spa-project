e82835cd980649ab8850769c35b2a42587d7c9c1
Areen <109797692+areenyaran@users.noreply.github.com>
2022-12-16 17:16:39 +0300

    Update StaffCards.js

--- src/components/HomePage/StaffHome/StaffCards.js
+++ src/components/HomePage/StaffHome/StaffCards.js
@@ -9,18 +9,14 @@ const StaffCards = () => {
       <div className="container d-flex justify-content-center">
         <div className="row  ">
           <div className=" col-md-3 col-lg-3 col-xxl-3">
-            <StaffCard img='./images/shayne.jpeg' alt1="shayne" title="Shayne Crocker" paragraph1="I'm Shane Crocker,48 years old.work in massage ,Skilled in various modalities 
-      of massage including Swedish, Deep Tissue/Neuromuscular"><SocialMedia1 /></StaffCard></div>
+            <StaffCard img='./images/shayne.jpeg' alt1="shayne" title="Shayne Crocker" paragraph1="I'm Shane Crocker,48 years old.work in massage ,Skilled in various modalities of massage including Swedish, Deep Tissue/Neuromuscular"><SocialMedia1 /></StaffCard></div>
           <div className=" col-md-3 col-lg-3 col-xxl-3">
-            <StaffCard img='./images/megan.jpg' alt1="megan" title="Megan Martin" paragraph1="I am Megan Martin,27 years old working in makeup ,Skilled in Swedish Massage, Deep Tissue Massage and Sports Massage.
-Practices integrative massage that combines techniques to form." ><SocialMedia2 /></StaffCard></div>
+            <StaffCard img='./images/megan.jpg' alt1="megan" title="Megan Martin" paragraph1="I am Megan Martin,27 years old working in makeup ,Skilled in Swedish Massage, Deep Tissue/Neuromuscular Massage and Sports Massage." ><SocialMedia2 /></StaffCard></div>
           <div className="col-md-3 col-lg-3 col-xxl-3">
-            <StaffCard img='./images/amanda.jpg' alt1="amanda" title="Amanda Mollett" paragraph1="I am Amanda Mollett,35 years old working in massage ,Specialize in Craniosacral, Neuromuscular,
-   Swedish, Deep Tissue, migraine and TMJD work."><SocialMedia1 /></StaffCard></div>
+            <StaffCard img='./images/amanda.jpg' alt1="amanda" title="Amanda Mollett" paragraph1="I am Amanda Mollett,35 years old working in massage ,Specialize in Craniosacral, Neuromuscular, Swedish, Deep Tissue, migraine and TMJD work."><SocialMedia1 /></StaffCard></div>
           <div className="col-md-3 col-lg-3 col-xxl-3">
             <StaffCard img='./images/lydia.jpg' alt1="lydia" title="Lydia Smothers" paragraph1="I am Lydia Smothers ,30 years old I have been working in massage for 10 years,Skilled in Swedish Massage, Deep Tissue Massage and Sports Massage."><SocialMedia2 /></StaffCard></div>
         </div></div></div>
   )
-
 }
 export default StaffCards;
