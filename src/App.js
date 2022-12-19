97908c216bffb16d73a3730281a2fdf2a92a5384
saifalaasabelaish <saifalaa.099@gmail.com>
2022-12-19 15:26:07 +0200

    admin pannel

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+//import TablesPage from "./components/Admin/TablesPage";
+
 import Admin from "./components/Admin/Admin"
 export default function App() {
+
   return (
-    <Admin/>
-  
+    <>
+    <Admin></Admin>
+   </>
   );
 }
 
--- src/components/Admin/AdminSideNavbar.js
+++ src/components/Admin/AdminSideNavbar.js
@@ -1,53 +1,30 @@
 const AdminSideNavbar = () => {
 
-    return (
-    
-        <nav className="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-        <div className="sb-sidenav-menu">
-          <div className="nav">
-            <div className="sb-sidenav-menu-heading">Core</div>
-            <a className="nav-link" href="index.html">
-              <div className="sb-nav-link-icon"><i className="fas fa-tachometer-alt" /></div>
-              Dashboard
-            </a>
-            <div className="sb-sidenav-menu-heading">Interface</div>
-            <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-              <div className="sb-nav-link-icon"><i className="fas fa-book-open" /></div>
-              Pages
-              <div className="sb-sidenav-collapse-arrow"><i className="fas fa-angle-down" /></div>
-            </a>
-            <div className="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-              <nav className="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                <a className="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-                  Authentication
-                  <div className="sb-sidenav-collapse-arrow"><i className="fas fa-angle-down" /></div>
-                </a>
-                <div className="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
-                  <nav className="sb-sidenav-menu-nested nav">
-                    <a className="nav-link" href="login.html">Login</a>
-                    <a className="nav-link" href="register.html">Register</a>
-                    <a className="nav-link" href="password.html">Forgot Password</a>
-                  </nav>
-                </div>
-              </nav>
-            </div>
-            <div className="sb-sidenav-menu-heading">Addons</div>
-            <a className="nav-link" href="charts.html">
-              <div className="sb-nav-link-icon"><i className="fas fa-chart-area" /></div>
-              Charts
-            </a>
-            <a className="nav-link" href="tables.html">
-              <div className="sb-nav-link-icon"><i className="fas fa-table" /></div>
-              Tables
-            </a>
-          </div>
+  return (
+
+    <nav className="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+      <div className="sb-sidenav-menu">
+        <div className="nav">
+          <a className="nav-link">
+            <div className="sb-nav-link-icon"><i className="fas fa-tachometer-alt" /></div>
+            Home Page
+          </a>
+          <a className="nav-link" >
+            <div className="sb-nav-link-icon"><i className="fas fa-chart-area" /></div>
+            Charts and Stats
+          </a>
+          <a className="nav-link">
+            <div className="sb-nav-link-icon"><i className="fas fa-table" /></div>
+            Data Base
+          </a>
         </div>
-        <div className="sb-sidenav-footer">
-          <div className="small">Logged in as:</div>
-          Admin
-        </div>
-      </nav>
-    );
-  }
+      </div>
+      <div className="sb-sidenav-footer">
+        <div className="small">Logged in as:</div>
+        Admin
+      </div>
+    </nav>
+  );
+}
 
 export default AdminSideNavbar;
