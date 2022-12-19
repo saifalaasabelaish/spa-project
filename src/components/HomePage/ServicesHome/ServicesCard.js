4fe400ae0b160fb93dc8ea1fa6c9af60202fee0a
Areen <109797692+areenyaran@users.noreply.github.com>
2022-12-19 22:07:03 +0200

    edit

--- src/components/HomePage/ServicesHome/ServicesCard.js
+++ src/components/HomePage/ServicesHome/ServicesCard.js
@@ -1,11 +1,14 @@
+import { Link } from "react-router-dom";
+
 const ServicesCard = (props) => {
   return (
-    <div className=" col-sm-6 col-md-4 col-lg-3">
-      <div className="services-card" >
+    <div className="col-sm-6 col-md-4 col-lg-3">
+      <div className="services-card">
         <img src={props.imag} alt="spa sevices"></img>
         <div className="card-body">
-          <h5 className="ser-card-title">{props.ser_card_title}</h5>
-          <p className="ser-card-text">{props.ser_card_text}</p>
+       <div className="text-center pt-3 text-white">
+        <Link to ="/ServicesPage" className="h5 text-white">{props.ser_card_title}</Link>
+         </div> <p className="ser-card-text">{props.ser_card_text}</p>
         </div>
       </div>
     </div>
