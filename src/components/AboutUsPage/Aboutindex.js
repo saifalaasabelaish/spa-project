320313ab5c1a9500834df52f38d2e3be52b02b99
lial-adam <s11926390@stu.najah.eduu>
2022-12-21 00:45:40 +0200

    About

---
+++ src/components/AboutUsPage/Aboutindex.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './GalleryData.css';
+import GalleryAbout from './App.js';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <GalleryAbout />
+  </React.StrictMode>
+);
