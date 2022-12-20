a123605ee349360b1b04e04fa011999682882576
saifalaasabelaish <saifalaa.099@gmail.com>
2022-12-21 01:17:56 +0200

    testing

--- src/components/BookingPage/BookNowButton.js
+++ src/components/BookingPage/BookNowButton.js
@@ -1,27 +1,10 @@
-import UserData from '../Admin/UserData';
 import './Booking.css'
-
-const day = document.getElementById("day-select");
-
-var filteredJson = UserData.filter(function (row) {
-    if (row.serviceDay == day) {
-        return true;
-    } else {
-        return false;
-    }
-});
-
-const Check = () => {
-if (filteredJson==1){
-    alert("Already Booked , Please Choose another day");}
-else {
-alert("Succesful");
-}}
-
 const BookNowButton = () => {
+
+
     return (
         <> <div className='booking'>
-            <button type="submit" className="btn btn-spa" onClick={Check}>Book Now</button>
+            <button type="submit" className="btn btn-spa">Book Now</button>
         </div>
         </>
     )
