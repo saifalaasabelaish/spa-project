f8f23f5903f0627696271c94c24edfc1a6e1dc2b
lial-adam <s11926390@stu.najah.eduu>
2022-12-21 00:49:11 +0200

    Team

---
+++ src/components/AboutUsPage/Team.js
@@ -0,0 +1,103 @@
+import './Team.css'
+/*
+const Team = () => {
+    return (
+     <div className="container">
+    
+     <div className="content">
+         <div className="columan">
+             <h3>TEAM</h3>
+             <p> " He who possesses beauty in the first place is the one who
+                possesses mercy and tolerance in his heart for others 
+                and for himself as well,as this beauty is
+                 what illuminates the heart and enchants the soul."
+               
+             </p>
+             <button type="button">Show More</button>
+             
+         </div>
+         <div className="columan">
+             <div className="Team t1">
+                 <h3>Alma Ali</h3>
+                 <img  src="/images/m1.png" alt="m1" />
+
+                 
+             </div>
+             <div className="Team t2">
+                 <h3>Ella Bader</h3>
+                 <img  src="/images/m2.png" alt="m2" />
+
+             </div>
+             <div className="Team t3">
+                 <h3>Ala Omar</h3>
+                 <img  src="/images/m3.png" alt="m3" />
+
+             </div>
+             <div className="Team t4">
+                 <h3>Hala Anas</h3>
+                 <img  src="/images/m4.png" alt="m4" />
+
+             </div>
+         </div>
+     </div>
+ </div>
+              
+    )
+ }
+ export default Team;
+ */
+ const Team = () => {
+    return (
+<div className="container">
+    
+    <div className="content">
+        <div className="columan">
+          
+            <p> " He who possesses beauty in the first place is the one who
+               possesses mercy and tolerance in his heart for others 
+               and for himself as well,as this beauty is
+                what illuminates the heart and enchants the soul."
+              
+            </p>
+            <button type="button">Show More</button>
+            
+       
+            <div className="wrapper">
+ <h1>Meet Our Team</h1>
+ <div className="team">
+   <div className="team_member">
+     <div className="team_img">
+       <img src="/images/m1.png" alt="Team_image"/>
+     </div>
+     <h3>Norhan Sameer</h3>
+     <p className="role">Nail expert</p>
+     <p>Nail expert and trainer with a certificate of professional practice (Russian manicure and gel techniques) from the Russian Varsi Nail School 🇷🇺🇮🇷.</p>
+   </div>
+   <div className="team_member">
+     <div className="team_img">
+       <img src="/images/m2.png" alt="Team_image"/>
+     </div>
+     <h3>Hala Abed Al Hadi</h3>
+     <p className="role">Hair expert</p>
+     <p>10 years experienceHe holds a certificate from the institute French beauty salon.</p></div>
+   <div className="team_member">
+     <div className="team_img">
+       <img src="/images/m3.png" alt="Team_image"/>
+     </div>
+     
+     <h3>ِAseel Alshaqi</h3>
+     <p className="role">Laser expert</p>
+     <p>Laser and hair removal expert with 4 years of experienceHe holds a laser technician certificate from the University of Technology.</p>
+   </div>
+ </div>
+</div>	
+        </div>
+    </div>
+</div>
+             
+              
+    )
+ }
+ export default Team;
+
+ 
