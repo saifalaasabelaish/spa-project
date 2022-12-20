e9e522b6f6e5351cd95eeddf2f0816f6e739654b
lial-adam <s11926390@stu.najah.eduu>
2022-12-21 00:44:08 +0200

    About

---
+++ src/components/AboutUsPage/About.js
@@ -0,0 +1,41 @@
+import "./GalleryData.css"
+import { GalleryData } from "./GalleryData";
+import { useEffect, useState } from "react";
+
+function GalleryAbout() {
+
+  const [data,setData] = useState([]);
+  const [collection,setCollection] = useState([]);
+
+  useEffect(()=>{
+    setData(GalleryData);
+    setCollection([... new Set(GalleryData.map((item)=> item.titile))])
+  },[]) 
+
+  const gallery_filter = (itemData) =>{
+    const filterData = GalleryData.filter((item)=> item.titile == itemData);
+    setData(filterData);
+  }
+
+  return (
+    <div className="GalleryAbout">
+      <div className="galleryWrapper">
+        <div className="filterItem">
+          <ul>
+            <li><button onClick={()=> setData(GalleryData)}>All</button></li>
+            {
+              collection.map((item)=> <li><button onClick={()=>{gallery_filter(item)}}>{item}</button></li>)
+            }
+          </ul>
+        </div>
+        <div className="galleryContainer">
+          {
+            data.map((item)=> <div  key={item.id} className="galleryItem"><img src={item.image  } /></div> )
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default GalleryAbout;
