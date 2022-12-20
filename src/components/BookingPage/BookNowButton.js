c180b9e8a651bd790c43d7810d16dfcfed05e101
saif alaa <saifalaa.099@gmail.com>
2022-12-20 23:07:21 +0200

    Booking

--- src/components/BookingPage/BookNowButton.js
+++ src/components/BookingPage/BookNowButton.js
@@ -1,10 +1,24 @@
 import './Booking.css'
+import {UserData} from "../Admin/UserData"
+const Check=()=>{
+    const day=document.getElementById("day-select").value;
+    const bookedDays=UserData.filter((el)=> el.serviceDay===day);
+    if(day===bookedDays)
+    return(
+        alert("Already Booked , Please Choose another day")
+    )
+
+
+
+}
 const BookNowButton = () => {
+
+
 
 
     return (
         <> <div className='booking'>
-            <button type="submit" className="btn btn-spa">Book Now</button>
+            <button type="submit" className="btn btn-spa" onClick={Check}>Book Now</button>
         </div>
         </>
     )
