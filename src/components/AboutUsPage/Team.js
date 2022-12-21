405433d3d38d0924dd043d5277a43fc7a52fd9c1
lial-adam <s11926390@stu.najah.eduu>
2022-12-21 17:51:53 +0200

    add social

--- src/components/AboutUsPage/Team.js
+++ src/components/AboutUsPage/Team.js
@@ -1,5 +1,4 @@
 import './Team.css'
-
  const Team = () => {
     return (
 <div className="container">
@@ -13,44 +12,104 @@ import './Team.css'
                 what illuminates the heart and enchants the soul."
               
             </p>
-            <button type="button">Show More</button>
+        
             
        
             <div className="wrapper">
-            <h4>
- " Here we have an elite team that is always brilliant and brilliant in this field of work "
- </h4>
+              
+ 
  <h1>Meet Our Team</h1>
  
  <div className="team">
    <div className="team_member">
      <div className="team_img">
        <img src="/images/m1.png" alt="Team_image"/>
+      
      </div>
+     
      <h3>Norhan Sameer</h3>
      <p className="role">Nail expert</p>
+    
      <p>Nail expert and trainer with a certificate of professional practice (Russian manicure and gel techniques) from the Russian Varsi Nail School 🇷🇺🇮🇷.</p>
+     <div className="button">
+            <div className="icon">
+               <i className="fab fa-facebook-f"></i>
+            </div>
+            <span>Facebook</span>
+         </div>
+         <div className="button">
+            <div className="icon">
+               <i className="fab fa-twitter"></i>
+            </div>
+            <span>Twitter</span>
+         </div>
+         <div className="button">
+            <div className="icon">
+               <i className="fab fa-instagram"></i>
+            </div>
+            <span>Instagram</span>
+         </div>
    </div>
    <div className="team_member">
      <div className="team_img">
        <img src="/images/m2.png" alt="Team_image"/>
+       
      </div>
      <h3>Hala Abed Al Hadi</h3>
      <p className="role">Hair expert</p>
-     <p>10 years experienceHe holds a certificate from the institute French beauty salon.</p></div>
+     <p>She holds the gold medal in hairdressing skill at the level of the French beauty salon, with years of experience.10 years experienceHe </p>
+     <div className="button">
+            <div className="icon">
+               <i className="fab fa-facebook-f"></i>
+            </div>
+            <span>Facebook</span>
+         </div>
+         <div className="button">
+            <div className="icon">
+               <i className="fab fa-twitter"></i>
+            </div>
+            <span>Twitter</span>
+         </div>
+         <div className="button">
+            <div className="icon">
+               <i className="fab fa-instagram"></i>
+            </div>
+            <span>Instagram</span>
+         </div></div>
    <div className="team_member">
      <div className="team_img">
        <img src="/images/m3.png" alt="Team_image"/>
+       
      </div>
      
      <h3>ِAseel Alshaqi</h3>
      <p className="role">Laser expert</p>
      <p>Laser and hair removal expert with 4 years of experienceHe holds a laser technician certificate from the University of Technology.</p>
+     <div className="button">
+            <div className="icon">
+               <i className="fab fa-facebook-f"></i>
+            </div>
+            <span>Facebook</span>
+         </div>
+         <div className="button">
+            <div className="icon">
+               <i className="fab fa-twitter"></i>
+            </div>
+            <span>Twitter</span>
+         </div>
+         <div className="button">
+            <div className="icon">
+               <i className="fab fa-instagram"></i>
+            </div>
+            <span>Instagram</span>
+         </div>
    </div>
+   
  </div>
-</div>	
-        </div>
+</div>
+</div>
     </div>
+    
 </div>
              
               
