305aae4ce05dada680d225820624b94e8664183f
lial-adam <s11926390@stu.najah.eduu>
2022-12-21 17:52:56 +0200

    nothing here just test somethng

--- src/components/Pages/AboutUsPage.js
+++ src/components/Pages/AboutUsPage.js
@@ -3,6 +3,7 @@ import Navbar from "../common/Navbar"
 import GalleryAbout from "../AboutUsPage/About"
 import Footer from "../common/Footer"
 import Team from "../AboutUsPage/Team"
+
 const AboutUsPage = () => {
     return (
         <>
