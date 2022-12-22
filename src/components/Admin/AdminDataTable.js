7815a933c30c93d4aba706b7c189bb28cc6b0589
saifalaasabelaish <saifalaa.099@gmail.com>
2022-12-22 20:32:05 +0200

    tst

--- src/components/Admin/AdminDataTable.js
+++ src/components/Admin/AdminDataTable.js
@@ -2,8 +2,8 @@ import UserData from './UserData'
 function AdminDataTable() {
     const DisplayData = UserData.map((info) => {
         return (
-            
-            <tr key={info.id}>/
+
+            <tr key={info.id}>
                 <td>{info.id}</td>
                 <td>{info.name}</td>
                 <td>{info.email}</td>
@@ -12,11 +12,6 @@ function AdminDataTable() {
                 <td>{info.service}</td>
                 <td>{info.serviceTime}</td>
                 <td>{info.pay}</td>
-                <td>
-                    <button type="submit" > Delete</button>
-                    <button type="submit" > Edit</button>
-
-                </td>
             </tr>
         )
     }
@@ -34,7 +29,6 @@ function AdminDataTable() {
                         <th> Services Booked</th>
                         <th> Service Time</th>
                         <th>Payment Method</th>
-                        <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody className="table-group-divider">
