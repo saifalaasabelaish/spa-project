030ae429e48ce924e6ecd9ed4b335066d04dc3e7
Areen <109797692+areenyaran@users.noreply.github.com>
2022-12-22 22:35:11 +0200

    edit

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     <>
       <div className="navbarr col-12">
         <nav className="navbar navbar-expand-lg navbar-expand-md text-end">
-          <div className="navbar-brand logo mt-2 pb-2">
+          <div className="navbar-brand logo ms-2">
             <Link to="/">
               <img src="/images/spalogo.png" alt="Logo" />
             </Link>
@@ -14,23 +14,23 @@ const Navbar = () => {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="main-navbar">
-            <ul className="navbar-nav ms-auto pb-3">
-              <li className="nav-item pt-2 pb-3">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
                 <Link to="/">Home</Link>
               </li>
-              <li className="nav-item pt-2 pb-3">
+              <li className="nav-item">
                 <Link to="/About">About Us</Link>
               </li>
-              <li className="nav-item pt-2 pb-3">
+              <li className="nav-item">
                 <Link to="/ShoppingPage">Shopping</Link>
               </li>
-              <li className="nav-item pt-2 pb-3">
+              <li className="nav-item">
                 <Link to="/ServicesPage">Services</Link>
               </li>
-              <li className="nav-item pt-2 pb-3">
+              <li className="nav-item">
                 <Link to="/Contact">Contact</Link>
               </li>
-              <li className="nav-item pt-2 pb-2">
+              <li className="nav-item pb-1">
                 <Link to="/BookingPage" className="bookingbtn">Book Now</Link>
               </li>
             </ul>
