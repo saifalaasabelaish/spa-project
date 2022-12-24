4c262b28663b803d11f23c5e44db8d6db2c12003
Roaajadaa <116982318+Roaajadaa@users.noreply.github.com>
2022-12-24 20:41:34 +0200

    Finally :)

--- src/components/ShoppingPage/ShoppingCard.js
+++ src/components/ShoppingPage/ShoppingCard.js
@@ -4,9 +4,9 @@ const ShoppingCard =(props)=>
 {
   const {addItem}=useCart()
 return(
-<div className="card border w-100">
+<div className="card border w-100" >
   <img src ={props.imgshoping}  className="card-img-top img-fluid"  alt="product"/>
-  <div className="card-body">
+  <div className="card-body" >
     <p className=" card-text d-flex justify-content-center productcard">  {props.content}  </p>
     <p className='d-flex justify-content-center'> {Intl.NumberFormat(undefined,{currency:"ILS" ,style:"currency"}).format( props.price)} </p>
    <div className='text-center'> <button className=" btncart" onClick={()=>addItem(props.product)}> Add To Cart </button> </div>
