a66b2b536710af37a2b06928bf2e39c37e7840dd
nourkalbouneh <116949296+nourkalbouneh@users.noreply.github.com>
2022-12-24 18:54:37 +0200

    java script and css code

---
+++ src/components/Gallery/Gallery.js
@@ -0,0 +1,6 @@
+const Gallery1 = (props) =>{
+    return(
+    <>
+    <img src={props.img} className="img img-fluid " alt={props.alt}></img>
+    </>)}
+    export default Gallery1;
---
+++ src/components/Gallery/GalleryPage.js
@@ -0,0 +1,177 @@
+import Gallery1 from './Gallery'
+import HeaderPage from "../common/HeaderPage";
+// import Footer from '../common/Footer';
+// import Navbar from '../common/Navbar';
+const Gallery = () => {
+  return (
+    <>
+      {/* <Navbar/> */}
+
+      <div className="row">
+
+        <Gallery1 img="./images/sp.png" alt="spa" /></div>
+      <HeaderPage title="Gallery" />
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6 col-sm-3"><Gallery1 img="./images/p1.png" alt="spa" /></div>
+          <div class="col-6 col-sm-3">
+            <div class="col-12 col-sm-3"><Gallery1 img="./images/p2.png" alt="spa" /></div>
+            <div class="row">
+              <div class="col-6 col-sm-3"> <img src="./images/p2.png" alt="spa" className='rounded-0 w-100 h-100 img img-fluid '></img></div>
+              <div class="col-6 col-sm-3"> <img src="./images/p2.png" alt="spa" className='rounded-0 img img-fluid '></img></div></div></div>
+          <div class="row">
+            <div class="col-6 col-sm-3">
+              <div class="row">
+                <div class="col-6 col-sm-3"> <img src="./images/p2.png" alt="spa" className='rounded-0 img img-fluid '></img></div>
+                <div class="col-6 col-sm-3"> <img src="./images/p2.png" alt="spa" className='rounded-0 img img-fluid '></img></div></div></div>
+            <div class="col-6 col-sm-3"><Gallery1 img="./images/p2.png" alt="spa" /></div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+      {/* <div className="row">
+
+        <Gallery1 img="./images/sp.png" alt="spa" /></div>
+      <HeaderPage title="Gallery" />
+      <div className="container text-center">
+        <div className="row">
+          <div className="col-6 ">
+           <img src = "./images/p2.png" alt="spa" className='rounded-0 img img-fluid '></img>
+            <Gallery1 img="./images/p4.png" alt="spa"></Gallery1>
+          </div>
+          <div className="col-6 ">
+            <Gallery1 img="./images/p3.png " alt="spa"></Gallery1>
+            <Gallery1 img="./images/p5.png" alt="spa"></Gallery1>
+            <Gallery1 img="./images/p9.png" alt="spa"></Gallery1>
+          </div>
+        </div></div>
+<div className="ratio ratio-16x9">
+        <iframe src="https://www.youtube.com/embed/kEgBo7gn_mE" title="YouTube video" allowfullscreen></iframe>
+      </div> */}
+
+
+
+
+
+
+
+
+      {/* 
+            
+          
+            <div className="row">
+              <div className="col-6 col-sm-3"></div>
+              <div className="col-6 col-sm-3"><Gallery1 img="./images/p4.png" alt="spa"></Gallery1></div></div></div>
+          <div className="row">
+            <div className="col-6 col-sm-3">
+              <div className="row">
+                <div className="col-6 col-sm-3"></div>
+                <div className="col-6 col-sm-3"><Gallery1 img="./images/p6.png" alt="spa"></Gallery1></div></div></div>
+            <div className="col-6 col-sm-3"><Gallery1 img="./images/p7.png" alt="spa"></Gallery1></div>
+          </div> */}
+
+
+
+
+
+      {/* <Footer/> */}
+
+    </>
+  )
+}
+export default Gallery;
+
+
+
+
+{/* <div className="ratio ratio-16x9">
+  <iframe src="https://www.youtube.com/embed/kEgBo7gn_mE" title="YouTube video" allowfullscreen></iframe>
+</div> col-sm-3 rounded-0*/}
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div className="">
+
+<div className="row">
+<Gallery1 img="./images/sp.png" alt ="spa"></Gallery1>
+</div>
+
+<div className="row">
+  <div className=' col-6'>
+<Gallery1 img="./images/p1.png" alt ="spa"></Gallery1></div></div>
+<Gallery1 img="./images/p2.png" alt ="spa"></Gallery1>
+<Gallery1 img="./images/p3.png" alt ="spa"></Gallery1>
+<Gallery1 img="./images/p4.png" alt ="spa"></Gallery1>
+<Gallery1 img="./images/p5.png" alt ="spa"></Gallery1>
+ {/* <Gallery1 img="./images/p6.png" alt ="spa"></Gallery1>
+<Gallery1 img="./images/p7.png" alt ="spa"></Gallery1>
+<Gallery1 img="./images/p8.png" alt ="spa"></Gallery1>
+<Gallery1 img="./images/p9.png" alt ="spa"></Gallery1>
+<Gallery1 img="./images/p10.png" alt ="spa"></Gallery1>
+<Gallery1 img="./images/p11.png" alt ="spa"></Gallery1>
+<Gallery1 img="./images/p12.png" alt ="spa"></Gallery1>
+<Gallery1 img="./images/p13.png" alt ="spa"></Gallery1>  
+
+</div>
+<div className="ratio ratio-16x9">
+  <iframe src="https://www.youtube.com/embed/kEgBo7gn_mE" title="YouTube video" allowfullscreen></iframe>
+</div> */}
+
+{/* <div className="contanier">
+
+<div className="row">
+  <div className="column">
+    <img src="..." alt="..." style="width:100%"></img>
+  </div>
+  <div className="column">
+    <img src="...." alt="..." style="width:100%"></img>
+  </div>
+  <div className="column">
+    <img src="...." alt="..." style="width:100%"></img>
+  </div>
+</div>
+
+<div className="row">
+  <div className="column">
+    <img src="...." alt="..." style="width:100%"></img>
+  </div>
+  <div className="column">
+    <img src="...." alt="..." style="width:100%"></img>
+  </div>
+  <div className="column">
+    <img src="......" alt="..." style="width:100%"></img>
+  </div>
+</div>
+
+<div className="row">
+  <div className="column">
+    <img src="...." alt="..." style="width:100%"></img>
+  </div>
+  <div className="column">
+    <img src="...." alt="..." style="width:100%"></img>
+  </div>
+  <div className="column">
+    <img src="......" alt="..." style="width:100%"></img>
+  </div>
+</div>
+
+
+
+
+</div> */}
+
